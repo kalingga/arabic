5e86f86bb9a3c267bdd9abd15630bf9f99bf3369
Khoirul Aksara <me@serat.us>
2026-03-14 20:11:39 +0700

    2026

--- ngarab.php
+++ ngarab.php
@@ -1,13 +1,30 @@
 <?php
 
 /*
-Plugin Name: (ng)Arab V2
-Plugin URI: http://lorosukmo.github.io/
-Description: dedicated to http://pelajarnujogja.or.id and http://ipnudiy.or.id - ^ I ❤ OpenSource ^
-Author: Khoirul Anwar a.k.a Loro Sukmo
-Version: 2.0
-Author URI: http://choiroel.wordpress.com/
+Plugin Name: (ng)Arab V3
+Plugin URI: https://log.serat.us
+Description: 
+Author: Khoirul Aksara
+Version: 3.0
+Author URI: https://log.serat.us
 */
+
+// Initialize GitHub Updater
+if ( is_admin() ) { // we only need this in the admin area
+	include_once( 'updater.php' );
+	$config = array(
+		'slug'               => plugin_basename( __FILE__ ),
+		'proper_folder_name' => 'arabic',
+		'api_url'            => 'https://api.github.com/repos/kalingga/arabic',
+		'raw_url'            => 'https://raw.githubusercontent.com/kalingga/arabic/master',
+		'github_url'         => 'https://github.com/kalingga/arabic',
+		'zip_url'            => 'https://github.com/kalingga/arabic/archive/master.zip',
+		'requires'           => '3.0',
+		'tested'             => '4.0',
+		'readme'             => 'README.md',
+	);
+	new WP_GitHub_Updater( $config );
+}
 
 //fungsi load css
 function makeup_arab() {
@@ -18,7 +35,7 @@ makeup_arab();
 
 //fungsi shortcode
 function shortcode_arab( $atts, $content = null ) {
-return '<p class="arab">' . $content . '</p>';
+	return '<div class="arab">' . wpautop( trim( $content ) ) . '</div>';
 }
 //hooking
 add_shortcode( 'ngarab', 'shortcode_arab' );
